15d9bbc29274d56bcb178aac3b65c7e189930e67
Priyanshu Dhariwal <priyanshudhariwal@gmail.com>
2025-09-09 00:40:21 +0530

    fixed execution bash script

--- execution.sh
+++ execution.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
+export LC_NUMERIC="C"
 
-NOXIM_BINARY_PATH = "../noxim/noxim/bin/noxim"
+if [ -z "$1" ]; then
+    echo "usage: ./execution.sh <filename.yaml>"
+    exit 1
+fi
 
-YAML_CONFIG_FILE = $1
+NOXIM_BINARY_PATH="../noxim/noxim/bin/noxim"
 
-FILENAME = $(basename "$YAML_CONFIG_FILE" .yaml)
-OUTPUT_FILE = "results_${FILENAME}.txt"
+YAML_CONFIG_FILE=$1
+
+FILENAME=$(basename "$YAML_CONFIG_FILE" .yaml)
+OUTPUT_FILE="results_${FILENAME}.txt"
+
+PIR_START=0.01
+PIR_STEP=0.01
+PIR_END=0.20
 
 rm -f "$OUTPUT_FILE"
 touch "$OUTPUT_FILE"
 
-PIR_START = 0.01
-PIR_STEP = 0.01
-PIR_END = 0.2
-
 for pir in $(seq $PIR_START $PIR_STEP $PIR_END)
 do
-    echo "--------------------------------------------------" tee -a "$OUTPUT_FILE"
-    echo "------------RUNNING FOR PIR = %.2f\n--------------" "$pir" | tee -a "$OUTPUT_FILE"
-    echo "--------------------------------------------------" tee -a "$OUTPUT_FILE"
+    echo "--------------------------------------------------" | tee -a "$OUTPUT_FILE"
+    printf "\n------------RUNNING FOR PIR = %.2f--------------\n" "$pir" | tee -a "$OUTPUT_FILE"
+    echo "--------------------------------------------------" | tee -a "$OUTPUT_FILE"
 
-    "$NOXIM_BINARY_PATH" - config "$YAML_CONFIG_FILE" -pir "$pir" >> "$OUTPUT_FILE"
+    "$NOXIM_BINARY_PATH" -config "$YAML_CONFIG_FILE" -pir "$pir" "poisson" >> "$OUTPUT_FILE"
+
+    #debugging statement
+    printf "$NOXIM_BINARY_PATH" -config "$YAML_CONFIG_FILE" -pir "$pir" "poisson" >> "$OUTPUT_FILE"
 
     printf "finished for PIR = %0.2f. \n \n" "$pir"
 done
